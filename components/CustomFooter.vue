570195f9ed58578f231b9f9b3939be40a5c069b0
Steele Shreve <steele@steelesh.dev>
2024-02-13 14:32:27 -0500

    refactor: remove service-specific pages and reference main service page instead

--- components/CustomFooter.vue
+++ components/CustomFooter.vue
@@ -125,9 +125,9 @@ const navigation = {
     { name: 'Contact', href: '/contact' }
   ],
   services: [
-    { name: 'Tax Preparation', href: '/services/preparation' },
-    { name: 'Tax Planning', href: '/services/planning' },
-    { name: 'Tax Compliance', href: '/services/compliance' }
+    { name: 'Tax Preparation', href: '/services#preparation' },
+    { name: 'Tax Planning', href: '/services#planning' },
+    { name: 'Tax Compliance', href: '/services#compliance' }
   ],
   legal: [
     { name: 'Accessibility', href: '/accessibility' },
--- pages/services/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <ServicesSection />
-</template>
