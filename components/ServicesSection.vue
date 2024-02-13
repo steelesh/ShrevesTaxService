52634dfb9757b16aaf3d0891c4b0bd26153fbb1e
Steele Shreve <steele@steelesh.dev>
2024-02-13 11:48:41 -0500

    feat: populate info about offered services

--- components/ServicesSection.vue
+++ components/ServicesSection.vue
@@ -4,22 +4,46 @@
       <span class="text-primary">Services</span>
       We Offer</template
     >
-    <UPageGrid>
-      <UPageCard
-        icon="i-heroicons-wrench-screwdriver"
-        title="Service 1"
-        description="Description of service 1"
-      />
-      <UPageCard
-        icon="i-heroicons-square-3-stack-3d"
-        title="Service 2"
-        description="Description of service 2"
-      />
-      <UPageCard
-        icon="i-heroicons-device-phone-mobile"
-        title="Service 3"
-        description="Description of service 3"
-      />
-    </UPageGrid>
+    <ULandingGrid>
+      <ULandingCard
+        class="col-span-4 row-span-2"
+        icon="i-heroicons-folder"
+        title="Tax Preparation"
+        description="Our tax preparation service ensures accurate and timely filing of individual and small business tax returns, guiding you through the process with expertise and attention to detail."
+        ><UButton
+          class="w-32"
+          to="/preparation"
+          :trailing="true"
+          icon="i-heroicons-arrow-long-right"
+          >Learn More</UButton
+        ></ULandingCard
+      >
+      <ULandingCard
+        class="col-span-4 row-span-2"
+        icon="i-heroicons-calculator"
+        title="Tax Planning"
+        description="Our tax planning service helps you minimize tax liability and maximize deductions through strategic planning tailored to your financial situation and goals."
+        ><UButton
+          class="w-32"
+          to="/planning"
+          :trailing="true"
+          icon="i-heroicons-arrow-long-right"
+          >Learn More</UButton
+        ></ULandingCard
+      >
+      <ULandingCard
+        class="col-span-4 row-span-2"
+        icon="i-heroicons-shield-check"
+        title="Tax Compliance"
+        description="We keep you informed and compliant with changing tax laws and regulations, ensuring you meet filing requirements and deadlines without stress or penalties."
+        ><UButton
+          class="w-32"
+          to="/compliance"
+          :trailing="true"
+          icon="i-heroicons-arrow-long-right"
+          >Learn More</UButton
+        ></ULandingCard
+      >
+    </ULandingGrid>
   </ULandingSection>
 </template>
