9a6d49773c77066c76b48b2df9d657f9268dbfea
Steele Shreve <steele@steelesh.dev>
2024-02-14 22:10:20 -0500

    feat: add pages for privacy policy and terms

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -15,8 +15,17 @@ const links = [
 
 <template>
   <UPage>
-    <UPageBody>
-      <UBreadcrumb :links="links" />
+    <UBreadcrumb class="mt-8" :links="links" :ui="{ ol: 'pl-0' }" />
+    <UPageHeader title="PRIVACY POLICY">
+      <template #description>
+        <p class="mb-4 text-sm"><i>Last updated February 14, 2024</i></p>
+        <p>
+          Please read this privacy policy carefully before using our website.
+        </p>
+      </template>
+    </UPageHeader>
+    <UPageBody prose>
+      <ContentDoc />
     </UPageBody>
   </UPage>
 </template>
--- pages/terms.vue
+++ pages/terms.vue
@@ -15,8 +15,18 @@ const links = [
 
 <template>
   <UPage>
-    <UPageBody>
-      <UBreadcrumb :links="links" />
+    <UBreadcrumb class="mt-8" :links="links" :ui="{ ol: 'pl-0' }" />
+    <UPageHeader title="TERMS AND CONDITIONS">
+      <template #description>
+        <p class="mb-4 text-sm"><i>Last updated February 14, 2024</i></p>
+        <p>
+          Please read these terms and conditions carefully before using our
+          website.
+        </p>
+      </template>
+    </UPageHeader>
+    <UPageBody prose>
+      <ContentDoc />
     </UPageBody>
   </UPage>
 </template>
