490f5865cdcda3fefb8e9807a0dd11fdb6b15933
Steele Shreve <steele@steelesh.dev>
2024-02-17 14:54:23 -0500

    feat: add wording for about section

--- pages/about.vue
+++ pages/about.vue
@@ -23,6 +23,43 @@ const links = [
       title="About Us"
       description="Get to know us and our background at Shreve's Tax Service."
     />
-    <UPageBody> </UPageBody>
+    <UPageBody
+      class="flex flex-col justify-between gap-10 lg:flex-row lg:gap-0"
+    >
+      <div class="flex flex-col gap-[20px] lg:w-[45%]">
+        <h2
+          class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl dark:text-white"
+        >
+          Our Story
+        </h2>
+        <p>
+          Shreve's Tax Service, a trusted family-owned business, has proudly
+          served the Zanesville community since 2001. Committed to excellence,
+          we specialize in providing top-tier tax preparation services tailored
+          to our clients' needs.
+        </p>
+        <p>
+          Led by Wendy Shreve, an authorized E-File Provider and dedicated
+          participant in the IRS Annual Filing Season Program, we uphold
+          standards of accuracy and compliance. Our personalized approach
+          ensures each client receives professional and friendly service,
+          guaranteed.
+        </p>
+        <p>
+          Wendy holds a Bachelor of Arts in Business Economics from Muskingum
+          College (1997). In addition to her role at Shreve's Tax Service, she
+          serves as the Fiscal Officer of Springfield Township and teaches 2nd
+          grade at Maysville High School.
+        </p>
+      </div>
+
+      <div class="w-[45%] self-center">
+        <NuxtImg
+          src="https://picsum.photos/1200/850"
+          alt="Wendy Shreve"
+          class="rounded-lg"
+        />
+      </div>
+    </UPageBody>
   </UPage>
 </template>
