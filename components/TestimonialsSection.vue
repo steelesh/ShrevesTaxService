294bfd534a2e217eaedbd12a37aeffbbeeb819ea
Steele Shreve <steele@steelesh.dev>
2024-02-18 13:26:08 -0500

    refactor: remove blank testimonials

--- components/TestimonialsSection.vue
+++ components/TestimonialsSection.vue
@@ -29,30 +29,6 @@
         }"
         card
       />
-      <ULandingTestimonial
-        class="col-span-6 row-span-4"
-        quote="Testimonial number 3"
-        :author="{
-          name: 'Jane Doe',
-          description: 'CEO',
-          to: 'https://google.com',
-          target: '_blank',
-          avatar: { src: 'https://i.pravatar.cc/120?img=1' }
-        }"
-        card
-      />
-      <ULandingTestimonial
-        class="col-span-6 row-span-2"
-        quote="Testimonial number 4"
-        :author="{
-          name: 'Jane Doe',
-          description: 'CEO',
-          to: 'https://google.com',
-          target: '_blank',
-          avatar: { src: 'https://i.pravatar.cc/120?img=1' }
-        }"
-        card
-      />
     </ULandingGrid>
   </ULandingSection>
 </template>
