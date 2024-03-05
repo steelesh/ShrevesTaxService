b301c68b5f72b06ead0c3e8a292ed2ce211c1666
Steele Shreve <steele@steelesh.dev>
2024-03-04 21:05:35 -0500

    refactor: make light mode the default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
     icons: ['heroicons', 'simple-icons']
   },
   colorMode: {
-    preference: 'dark'
+    preference: 'light'
   },
   devtools: {
     enabled: true
