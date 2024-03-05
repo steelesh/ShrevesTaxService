09e30469afb3e1500a32c5de331ecdf78015971e
Steele Shreve <steele@steelesh.dev>
2024-03-05 12:19:25 -0500

    refactor: make services section more readable

--- components/ServicesSection.vue
+++ components/ServicesSection.vue
@@ -2,55 +2,46 @@
   <ULandingSection
     headline="Services"
     title="What We Can Do For You"
-    description="Our services are designed to meet the needs of individuals and small businesses, providing guidance and support for tax preparation, planning, and compliance. Wendy ensures that each client receives personalized attention and thorough assistance."
     id="services"
     class="section"
     v-motion-fade
     :delay="100"
   >
+    <template #description>
+      <span
+        >Our services are designed to meet the needs of individuals and small
+        businesses, providing guidance and support for tax preparation,
+        planning, and compliance. Wendy ensures that each client receives
+        personalized attention and thorough assistance.</span
+      >
+      <br />
+      <br />
+      <span
+        >To read more about our services,
+        <NuxtLink class="text-primary" to="/services"
+          >click here</NuxtLink
+        ></span
+      >
+    </template>
     <ULandingGrid>
       <ULandingCard
         class="col-span-4 row-span-2"
         icon="i-heroicons-folder"
         title="Tax Preparation"
         description="Our tax preparation service ensures accurate and timely filing of individual and small business tax returns, guiding you through the process with expertise and attention to detail."
-        ><UButton
-          class="w-32"
-          to="/services#tax-preparation"
-          :trailing="true"
-          icon="i-heroicons-arrow-long-right"
-          aria-label="Learn more about our tax preparation service"
-          >Read More</UButton
-        ></ULandingCard
-      >
+      ></ULandingCard>
       <ULandingCard
         class="col-span-4 row-span-2"
         icon="i-heroicons-calculator"
         title="Tax Planning"
         description="Our tax planning service helps you minimize tax liability and maximize deductions through strategic planning tailored to your financial situation and goals."
-        ><UButton
-          class="w-32"
-          to="/services#tax-planning"
-          :trailing="true"
-          icon="i-heroicons-arrow-long-right"
-          aria-label="Learn more about our tax planning service"
-          >Read More</UButton
-        ></ULandingCard
-      >
+      ></ULandingCard>
       <ULandingCard
         class="col-span-4 row-span-2"
         icon="i-heroicons-shield-check"
         title="Tax Compliance"
         description="We keep you informed and compliant with changing tax laws and regulations, ensuring you meet filing requirements and deadlines without stress or penalties."
-        ><UButton
-          class="w-32"
-          to="/services#tax-compliance"
-          :trailing="true"
-          icon="i-heroicons-arrow-long-right"
-          aria-label="Learn more about our tax compliance service"
-          >Read More</UButton
-        ></ULandingCard
-      >
+      ></ULandingCard>
     </ULandingGrid>
   </ULandingSection>
 </template>
